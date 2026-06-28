68eb0c4ff95f6164e5e58c86161a02521c5a33a3
Darkroa <92130304+Darkroa@users.noreply.github.com>
2026-06-28 17:57:17 +0100

    Initial commit

--- src/frontend/pages/ban.py
+++ src/frontend/pages/ban.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🚫 Account Banned")
 
-st.error("Your account has been banned by the administrator.")
+st.error("Your account has been banned  for user violations.")
 st.write("If you believe this is a mistake, please contact support.")
 
 if st.button("Logout"):
